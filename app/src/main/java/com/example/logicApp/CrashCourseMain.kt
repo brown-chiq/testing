af9e5bc6b2be0132462c8a53703366cd3db4f0be
brownn_chiq <samy0005@e.ntu.edu.sg>
2023-11-17 23:50:10 +0800

    homepage UI, buttons not functional yet

---
+++ app/src/main/java/com/example/logicApp/CrashCourseMain.kt
@@ -0,0 +1,12 @@
+package com.example.logicApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.logikit1.R
+
+class CrashCourseMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crash_course_main)
+    }
+}
